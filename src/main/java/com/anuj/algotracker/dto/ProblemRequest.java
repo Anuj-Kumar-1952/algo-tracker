ee499bcd7b1c08e1b12ca536b47b835a3f02b46c
Anuj-Kumar-1952 <ak1952002@gmail.com>
2025-12-18 19:15:11 +0530

    Add ProblemResponse & ProblemRequest DTOs

---
+++ src/main/java/com/anuj/algotracker/dto/ProblemRequest.java
@@ -0,0 +1,36 @@
+package com.anuj.algotracker.dto;
+
+import com.anuj.algotracker.model.Difficulty;
+import com.anuj.algotracker.model.ProblemStatus;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ProblemRequest {
+
+    @NotBlank
+    private String title;
+
+    private String description;
+
+    @NotNull
+    private Difficulty difficulty;
+
+    @NotBlank
+    private String topic;
+
+    private String link;
+
+    private ProblemStatus status;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
