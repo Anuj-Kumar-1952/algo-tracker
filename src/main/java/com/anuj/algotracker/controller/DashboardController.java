f093700e6e26d575d863152b8334b73a16b6b329
Anuj-Kumar-1952 <ak1952002@gmail.com>
2025-12-22 13:37:10 +0530

    Enhance controllers with Swagger annotations for secured APIs

--- src/main/java/com/anuj/algotracker/controller/DashboardController.java
+++ src/main/java/com/anuj/algotracker/controller/DashboardController.java
@@ -2,12 +2,16 @@ package com.anuj.algotracker.controller;
 
 import com.anuj.algotracker.dto.DashboardSummary;
 import com.anuj.algotracker.service.DashboardService;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/dashboard")
+@SecurityRequirement(name = "bearerAuth")
 public class DashboardController {
 
     private final DashboardService dashboardService;
